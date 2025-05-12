ffc876bb8b580c068c94cd42e92bd477ebdd4e08
amir <amirtaki623@gmail.com>
2025-05-12 19:01:24 +0330

    compelete project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-const router =  ()=>{
+
+window.addEventListener("hashchange",(event)=>{
+    // console.log(location.hash)
+    router()
+
+})
+
+function router (){
    switch (location.hash){
     case "":
         render ({title : "HOME", color : 'red', href : "#home"})
@@ -12,7 +19,7 @@ const router =  ()=>{
         render ({title: "SEARCH", color : 'blue', href : "#search"})
         break;
 
-    case "#LIKES" :
+    case "#likes" :
         render ({title : "LIKES", color : "yellow", href : "#likes"})
         break;
 
@@ -22,7 +29,14 @@ const router =  ()=>{
     }
 }
 
-window.addEventListener("hashchange",(event)=>{
-    console.log(location.hash)
-})
+function render  (data) {
+ 
+    document.querySelector("h1").innerText = data.title
+    document.querySelector(".container_home").style.backgroundColor = data.color
+ 
+    document.querySelectorAll('A').forEach((element) => {
+      
+        element.style.color = element.href.includes(data.href) ? "silver" : "black"
+    });
+}
 
